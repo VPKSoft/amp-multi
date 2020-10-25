b3992a1d36218bd24f308cc78f7379962dd62ce9
VPKSoft <vpksoft@vpksoft.net>
2020-10-25 18:05:16 +0200

    Fix missing signtool?

--- InstallerBaseWixSharp/github_release.ps1
+++ InstallerBaseWixSharp/github_release.ps1
@@ -28,6 +28,8 @@ $output_file = "amp\CryptEnvVar.exe"
 
 $download_url = "https://www.vpksoft.net/toolset/CryptEnvVar.exe"
 
+$output_file_signtool = "signtool.exe"
+
 Write-Output "Download file:  $download_url ..."
 # No need to remove this: Remove-Item $output_file
 (New-Object System.Net.WebClient).DownloadFile($download_url, $output_file)
@@ -40,6 +42,8 @@ $environment_cryptor = "CryptEnvVar.exe"
 # create the digital signature..
 $arguments = @("-s", $Env:SECRET_KEY, "-e", "CERT_1;CERT_2;CERT_3;CERT_4;CERT_5;CERT_6;CERT_7;CERT_8", "-f", "C:\vpksoft.pfx", "-w", "80", "-i", "-v")
 & (-join($application, "\", $environment_cryptor)) $arguments
+
+$signtool = (-join($application, "\", $output_file_signtool))
 
 # register the certificate to the CI image..
 $certpw=ConvertTo-SecureString $Env:PFX_PASS –asplaintext –force 
@@ -61,7 +65,7 @@ $release_exe = "..\amp\bin\Release\net47\win10-x64\amp.exe"
         $arguments = @("sign", "/f", "C:\vpksoft.pfx", "/p", $Env:PFX_PASS, "/fd", "sha256", "/tr", "http://timestamp.comodoca.com/?td=sha256", "/td", "sha256", "/as", "/v", $file)
 
 	    # on the second time, something about 'Keyset does not exist'. TODO::Clean temp?
-        signtool.exe $arguments > null 2>&1
+        & $signtool $arguments > null 2>&1
 	    Write-Output (-join("Installer package (MSI) signed: ", $file, "."))
 
         # After signing, clean up the temporary folder, if this helps with the multiple package signing..
@@ -73,7 +77,7 @@ $release_exe = "..\amp\bin\Release\net47\win10-x64\amp.exe"
         $arguments = @("sign", "/f", "C:\vpksoft.pfx", "/p", $Env:PFX_PASS, "/t", "http://timestamp.comodoca.com/?td=sha256", "/v", $file)
 
 	    # on the second time, something about 'Keyset does not exist'. TODO::Clean temp?
-        signtool.exe $arguments > null 2>&1
+        & $signtool $arguments > null 2>&1
 	    Write-Output (-join("Installer package (MSI) signed: ", $file, "."))
 
         # After signing, clean up the temporary folder, if this helps with the multiple package signing..
