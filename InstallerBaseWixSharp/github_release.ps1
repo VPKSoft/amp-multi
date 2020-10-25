9fb169d3bb386ee346fbf12e82ceb33405321ea7
VPKSoft <vpksoft@vpksoft.net>
2020-10-25 18:12:12 +0200

    Fixed missing signtool r.2.

--- InstallerBaseWixSharp/github_release.ps1
+++ InstallerBaseWixSharp/github_release.ps1
@@ -35,6 +35,14 @@ Write-Output "Download file:  $download_url ..."
 (New-Object System.Net.WebClient).DownloadFile($download_url, $output_file)
 Write-Output "Download done."
 
+$output_file_signtool = "amp\CryptEnvVar.exe"
+$download_url = "https://www.vpksoft.net/toolset/signtool.exe"
+
+Write-Output "Download file:  $output_file_signtool ..."
+# No need to remove this: Remove-Item $output_file
+(New-Object System.Net.WebClient).DownloadFile($output_file_signtool, $output_file_signtool)
+Write-Output "Download done."
+
 # application parameters..
 $application = "amp"
 $environment_cryptor = "CryptEnvVar.exe"
