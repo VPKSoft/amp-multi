81f1c1613376b3180b5615dd256b0b5d30992d3c
VPKSoft <vpksoft@vpksoft.net>
2020-10-26 18:56:04 +0200

    Signtool fixed?

--- InstallerBaseWixSharp/github_release.ps1
+++ InstallerBaseWixSharp/github_release.ps1
@@ -49,8 +49,6 @@ $environment_cryptor = "CryptEnvVar.exe"
 $arguments = @("-s", $Env:SECRET_KEY, "-e", "CERT_1;CERT_2;CERT_3;CERT_4;CERT_5;CERT_6;CERT_7;CERT_8", "-f", "C:\vpksoft.pfx", "-w", "80", "-i", "-v")
 & (-join($application, "\", $environment_cryptor)) $arguments
 
-$signtool = (-join($application, "\", $output_file_signtool))
-
 # register the certificate to the CI image..
 $certpw=ConvertTo-SecureString $Env:PFX_PASS –asplaintext –force 
 Import-PfxCertificate -FilePath "C:\vpksoft.pfx" -CertStoreLocation Cert:\LocalMachine\My -Password $certpw | Out-Null
@@ -73,7 +71,7 @@ if (![string]::IsNullOrEmpty($Env:CIRCLE_TAG)) # only release for tags..
         $arguments = @("sign", "/f", "C:\vpksoft.pfx", "/p", $Env:PFX_PASS, "/fd", "sha256", "/tr", "http://timestamp.comodoca.com/?td=sha256", "/td", "sha256", "/as", "/v", $file)
 
 	    # on the second time, something about 'Keyset does not exist'. TODO::Clean temp?
-        & $signtool $arguments # > null 2>&1
+        & $output_file_signtool $arguments # > null 2>&1
 	    Write-Output (-join("Installer package (MSI) signed: ", $file, "."))
 
         # After signing, clean up the temporary folder, if this helps with the multiple package signing..
@@ -85,7 +83,7 @@ if (![string]::IsNullOrEmpty($Env:CIRCLE_TAG)) # only release for tags..
         $arguments = @("sign", "/f", "C:\vpksoft.pfx", "/p", $Env:PFX_PASS, "/t", "http://timestamp.comodoca.com/?td=sha256", "/v", $file)
 
 	    # on the second time, something about 'Keyset does not exist'. TODO::Clean temp?
-        & $signtool $arguments #> null 2>&1
+        & $output_file_signtool $arguments #> null 2>&1
 	    Write-Output (-join("Installer package (MSI) signed: ", $file, "."))
 
         # After signing, clean up the temporary folder, if this helps with the multiple package signing..
