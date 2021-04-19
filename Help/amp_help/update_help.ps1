0b6afdf5d21d5aac3e0885fa8be3a6f46174e6c1
VPKSoft <vpksoft@vpksoft.net>
2021-04-19 19:13:20 +0300

    Test help build.

--- Help/amp_help/update_help.ps1
+++ Help/amp_help/update_help.ps1
@@ -2,3 +2,8 @@ $wiki_folder = ".\amp.wiki"
 Remove-Item -Path $wiki_folder -Recurse -Force -ErrorAction Ignore
 git clone https://github.com/VPKSoft/amp.wiki.git
 Copy-Item ".\amp.wiki\*.md" ".\docs"
+mkdocs build
+$help_outpath = "..\..\amp\bin\Release\net5.0-windows\win10-x64\Help"
+Remove-Item -Path $help_outpath -Recurse -Force -ErrorAction Ignore
+mkdir -Path $help_outpath
+Copy-Item .\site\*.* -Destination $help_outpath
