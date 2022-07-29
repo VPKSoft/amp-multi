baebb77af5f29f179514f628eaf43eab197189d8
VPKSoft <vpksoft@vpksoft.net>
2022-07-29 21:39:30 +0300

    Initial release script and some adjustments.

--- Packaging/Linux/AppImage/release.sh
+++ Packaging/Linux/AppImage/release.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 mkdir -p ./AppDir/usr/bin/ 
 dotnet publish ../../../amp.EtoForms/amp.EtoForms.csproj -c Release --runtime linux-x64 --output ./AppDir/usr/bin/ --self-contained
+wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
+chmod a+x appimagetool-x86_64.AppImage
+./appimagetool-x86_64.AppImage ./AppDir
