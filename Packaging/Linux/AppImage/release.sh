71cbe16ab24150cfda10974688d2d23f7ce4c096
VPKSoft <vpksoft@vpksoft.net>
2022-07-30 12:06:18 +0300

    Add directory permission command.

--- Packaging/Linux/AppImage/release.sh
+++ Packaging/Linux/AppImage/release.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+chmod 755 ./AppDir/AppRun
 mkdir -p ./AppDir/usr/bin/ 
 dotnet publish ../../../amp.EtoForms/amp.EtoForms.csproj -c Release --runtime linux-x64 --output ./AppDir/usr/bin/ --self-contained
 wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
