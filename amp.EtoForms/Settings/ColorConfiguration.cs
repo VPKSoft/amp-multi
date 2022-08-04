88a35486fcf006926ae3caa59bd2a33fd85f4ee5
VPKSoft <vpksoft@vpksoft.net>
2022-08-04 06:51:14 +0300

    Drafting.

---
+++ amp.EtoForms/Settings/ColorConfiguration.cs
@@ -0,0 +1,50 @@
+﻿#region License
+/*
+MIT License
+
+Copyright(c) 2022 Petteri Kautonen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eto.Drawing;
+using VPKSoft.ApplicationSettingsJson;
+
+namespace amp.EtoForms.Settings;
+
+/// <summary>
+/// A color configuration class for the amp#.
+/// Implements the <see cref="ApplicationJsonSettings" />
+/// </summary>
+/// <seealso cref="ApplicationJsonSettings" />
+public class ColorConfiguration : ApplicationJsonSettings
+{
+    /// <summary>
+    /// Gets or sets the color of the play-pause toggle button.
+    /// </summary>
+    /// <value>The color of the play-pause toggle button.</value>
+    [Settings(Default = "800080")] // Purple
+    public string PlayPauseButtonColor { get; set; } = string.Empty;
+}
